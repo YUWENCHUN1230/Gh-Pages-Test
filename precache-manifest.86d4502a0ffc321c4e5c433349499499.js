eeebb39a6fb1a9c3d1f60fd20d5a48312e13cfb2
Rubio <yurubio@mail.apex.com.tw>
2020-01-30 14:53:04 +0800

    Updates

---
+++ precache-manifest.86d4502a0ffc321c4e5c433349499499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4417b66eae1972c6d8ad5ee074dccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d6bd6aedd142186175",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "74d6bd6aedd142186175",
+    "url": "/static/js/main.44872c8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
